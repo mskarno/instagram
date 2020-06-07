e0d68773fcb5e921729895f759271f5be5da2473
arno <mskarno@users.noreply.github.com>
2020-06-07 17:12:02 +0700

    Create main.py

---
+++ .github/alur kerja/main.py
@@ -0,0 +1,22 @@
+# ani
+Instagram media downloader
+# Instalation
+```
+pkg update && pkg upgrade
+pkg install python git
+git clone https://github.com/karjok/ani
+cd ani
+pip install -r requirements.txt
+
+```
+# Usage
+```
+cd ani
+python ani.py
+type 'help' for more info
+```
+# Find me on
+<a href='https://fb.me/om.karjok' target='blank'>Facebook</a></br>
+<a href='https://t.me/om_karjok'>Telegram</a></br>
+<a href='https://www.instagram.com/karjok.pangesty' target='blank'>Instagram</a></br>
+or on <a href='https://t.me/CRABS_ID'>Cracker Noob Squads (CRABS) telegram group</a>
